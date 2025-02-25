ea26a340cd337e31c64ff1d642c8b1acce1501b7
Saurabh Pandey <saurabh.pandey@herovired.com>
2025-02-25 22:53:33 +0530

    more changes

--- index.js
+++ index.js
@@ -3,28 +3,13 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 6060;
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
-const privateKey = fs.readFileSync("private.pem", "utf8");
-const { generateFreshdeskJWT } = require("./generateFreshdeskJWT");
+// const cors = require("cors");
+// const jwt = require("jsonwebtoken");
+// const fs = require("fs");
+// const privateKey = fs.readFileSync("private.pem", "utf8");
+// const { generateFreshdeskJWT } = require("./generateFreshdeskJWT");
 const connectDB = require("./connectDB");
 const Data = require("./Data");
-
-app.get("/freshdesk/sso", (req, res) => {
-  const user = {
-    name: req.query.name || "Saurabh Pandey",
-    email: req.query.email || "saurabh.pandey@herovired.com",
-  };
-
-  console.log(user, "111111111111111111111");
-
-  const token = generateFreshdeskJWT(user);
-  const redirectUrl = `https://herovired.freshdesk.com/support/tickets/49241?jwt=${token}`;
-  console.log(redirectUrl, "222222222222222222222");
-  res.status(200).json({ redirectionUrl: redirectUrl });
-  // res.redirect(redirectUrl);
-});
 
 app.get("/generate-jwt", (req, res) => {
   try {
